eef3e51a71ead164092f29b3d1ec6011538d5dfe
thomasdubost <thomas.dubost@etu.u-paris.fr>
2025-12-15 15:13:48 +0100

    added swapAnimal

--- podium.c
+++ podium.c
@@ -1,5 +1,6 @@
 #include<string.h>
 #include "podium.h"
+#define MAX_TMP 10
 
 void addAnimal(Podium* podium, Animal animal)
 {
@@ -8,12 +9,14 @@ void addAnimal(Podium* podium, Animal animal)
 
 void moveAnimal(Podium* src, Podium* dest)
 {    
-     addAnimal(dest,src->animals[--src->nb_animals]);     
+     addAnimal(dest,src->animals[--src->nb_animals]);  
+     --src->nb_animals;
 }
 
 void swapAnimal(Podium* podium1, Podium* podium2)
 {
+     char tmp[MAX_TMP];
      strcpy(tmp, podium1->animals[podium1->nb_animals-1]);
-     strcpy(podium1->animals[podium1->nb_animals],);
-
+     strcpy(podium1->animals[podium1->nb_animals], podium2->animals[podium2->nb_animals-1]);
+     strcpy(podium2->animals[podium2->nb_animals], tmp);
 }
